3d2ecd47d8f77c7b2785bfa4a396f7fb3e0f2059
RalfG <ralfg@hotmail.be>
2019-12-17 21:33:25 +0100

    First check if requirements passed

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,8 +8,11 @@ BUILD_REQUIREMENTS=$2
 # Compile wheels
 arrPY_VERSIONS=(${PY_VERSIONS// / })
 for PY_VER in "${arrPY_VERSIONS[@]}"; do
-    /opt/python/${PY_VER}/bin/pip install ${BUILD_REQUIREMENTS} || { echo "\nInstalling requirements failed."; exit 1; }
-    /opt/python/${PY_VER}/bin/pip wheel /github/workspace/ -w /github/workspace/wheelhouse/ || { echo "\nBuilding wheels failed."; exit 1; }
+    # Check if requirements were passed
+    if [ ! -z "$2" ]; then
+        /opt/python/${PY_VER}/bin/pip install ${BUILD_REQUIREMENTS} || { echo "Installing requirements failed."; exit 1; }
+    fi
+    /opt/python/${PY_VER}/bin/pip wheel /github/workspace/ -w /github/workspace/wheelhouse/ || { echo "Building wheels failed."; exit 1; }
 done
 
-echo "\nSuccesfully build wheels"
+echo "Succesfully build wheels"
